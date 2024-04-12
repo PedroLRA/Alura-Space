09a11eec00a1038d1e5bc9da15952f267a99ea59
PedroLRA <pedro.lralm@gmail.com>
2024-04-12 23:01:19 +0100

    feat: Changed Photo.photoPath to media files

--- galery/models.py
+++ galery/models.py
@@ -13,10 +13,10 @@ class Photo(models.Model):
     name: str = models.CharField(max_length=100, null=False, blank=False)
     subtitle: str = models.CharField(max_length=150, null=False, blank=False)
     description: str = models.TextField(null=False, blank=False)
-    photoPath: str = models.CharField(max_length=150, null=False, blank=False)
+    photoPath: str = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     category: str = models.CharField(max_length=50, choices=CATEGORIES, default='')
     date: datetime = models.DateTimeField(default=datetime.now, blank=False)
     publish: bool = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Photo [name={self.name}]"
+        return self.name
