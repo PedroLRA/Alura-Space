909bc9092c88fbc5c4fde7288c4fd5d8a76dff6e
PedroLRA <pedro.lralm@gmail.com>
2024-04-12 18:24:55 +0100

    feat: Added 'category' attribute to Photo class

--- galery/admin.py
+++ galery/admin.py
@@ -5,6 +5,8 @@ class ListPhotos(admin.ModelAdmin):
     list_display: list = ["id", "name", "subtitle"]
     list_display_links: list = ["id", "name"]
     search_fields: list = ["name"]
+    list_filter: list = ["category"]
+    list_per_page = 10
 
 admin.site.register(Photo, ListPhotos)
 
--- galery/models.py
+++ galery/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 class Photo(models.Model):
+
+    CATEGORIES = [
+        ("NEBULA", "Nebula"),
+        ("STAR", "Star"),
+        ("GALAXY", "Galaxy"),
+        ("planet", "Planet"),
+    ]
+
     name: str = models.CharField(max_length=100, null=False, blank=False)
     subtitle: str = models.CharField(max_length=150, null=False, blank=False)
     description: str = models.TextField(null=False, blank=False)
     photoPath: str = models.CharField(max_length=150, null=False, blank=False)
+    category: str = models.CharField(max_length=50, choices=CATEGORIES, default='')
 
     def __str__(self):
         return f"Photo [name={self.name}]"
