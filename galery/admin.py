db2dc9201e1c2d453d618592b716729d87ae8704
PedroLRA <pedro.lralm@gmail.com>
2024-04-12 17:58:42 +0100

    feat: Added CRUD to admin page

--- galery/admin.py
+++ galery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galery.models import Photo
 
-# Register your models here.
+class ListPhotos(admin.ModelAdmin):
+    list_display: list = ["id", "name", "subtitle"]
+    list_display_links: list = ["id", "name"]
+    search_fields: list = ["name"]
+
+admin.site.register(Photo, ListPhotos)
+
