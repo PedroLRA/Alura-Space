7eef97ea72010c42aba5f053d1181894f9e97916
PedroLRA <pedro.lralm@gmail.com>
2024-04-12 02:46:56 +0100

    feat: Creating photo table in the DB

--- galery/models.py
+++ galery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    name: str = models.CharField(max_length=100, null=False, blank=False)
+    subtitle: str = models.CharField(max_length=150, null=False, blank=False)
+    description: str = models.TextField(null=False, blank=False)
+    photoPath: str = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f"Photo [name={self.name}]"
