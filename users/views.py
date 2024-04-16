6ccc9239e94abd844d8061c3f8f0ec12e4f8908c
PedroLRA <pedro.lralm@gmail.com>
2024-04-16 01:36:33 +0100

    feat: Implemented registration logic

--- users/views.py
+++ users/views.py
@@ -6,5 +6,32 @@ def login(request):
     return render(request, 'users/login.html', {'form': form})
 
 def register(request):
+    if request.method == 'POST':
+        form = RegisterForms(request.POST)
+    
+        if not form.is_valid(): # form info is invalid
+            return redirect('register')
+
+        username = form['username'].value()
+        email = form['email'].value()
+        password = form['password'].value()
+        passwordConfirm = form['passwordConfirm'].value()
+        
+        if password != passwordConfirm: # password confirmation check
+            return redirect('register')
+
+        #if user already exists
+        if User.objects.filter(username=username).exists(): 
+            return redirect('register')
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        user.save()
+        
+        return redirect('login')
+    
     form = RegisterForms()
     return render(request, 'users/register.html', {'form': form})
