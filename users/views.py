6510b58fa570bc237d6c5e4bec22440176382338
PedroLRA <pedro.lralm@gmail.com>
2024-04-16 01:36:52 +0100

    feat: Implemented login logic

--- users/views.py
+++ users/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 from users.forms import LoginForms, RegisterForms
 
 def login(request):
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if not form.is_valid(): # form info is invalid
+            return redirect('login')
+
+        username = form['username'].value()
+        password = form['password'].value()
+
+        user = auth.authenticate(request, username=username, password=password)
+        
+        if user is None: # User not authenticated
+            return redirect('login')
+        
+        auth.login(request, user=user)
+        return redirect('index')
+
     form = LoginForms()
     return render(request, 'users/login.html', {'form': form})
 
