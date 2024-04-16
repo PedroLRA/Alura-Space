dd3eff81b776973786742dd871c07f9b17a5b53c
PedroLRA <pedro.lralm@gmail.com>
2024-04-16 21:38:29 +0100

    feat: Added user auth check and conditional rendering

--- galery/views.py
+++ galery/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from galery.models import Photo
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não conectado')
+        return redirect('login')
+    
     photos = Photo.objects.filter(publish=True).order_by("-date")
 
     # Filtering by search html query
@@ -12,5 +17,9 @@ def index(request):
     return render(request, 'galery/index.html', {"photos": photos})
 
 def image(request, photoId):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não conectado')
+        return redirect('login')
+        
     photo = get_object_or_404(Photo, pk=photoId)
     return render(request, 'galery/image.html', {"photo": photo})
