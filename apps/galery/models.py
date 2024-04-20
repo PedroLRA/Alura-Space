6f9c2afe0a2131e1fcc78fdc871bf44ef70efbac
PedroLRA <pedro.lralm@gmail.com>
2024-04-20 20:29:27 +0100

    fix: Typos on Photo model

--- apps/galery/models.py
+++ apps/galery/models.py
@@ -8,7 +8,7 @@ class Photo(models.Model):
         ("NEBULA", "Nebula"),
         ("STAR", "Star"),
         ("GALAXY", "Galaxy"),
-        ("planet", "Planet"),
+        ("PLANET", "Planet"),
     ]
 
     name: str = models.CharField(max_length=100, null=False, blank=False)
@@ -17,7 +17,7 @@ class Photo(models.Model):
     photoPath: str = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     category: str = models.CharField(max_length=50, choices=CATEGORIES, default='')
     date: datetime = models.DateTimeField(default=datetime.now, blank=False)
-    publish: bool = models.BooleanField(default=False)
+    publish: bool = models.BooleanField(default=True)
     user: User = models.ForeignKey(
         to=User,
         on_delete=models.SET_NULL,
